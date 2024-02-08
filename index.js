0c40ff6e5e5a47e23d01b876e3d6793284b7c060
IND-SAB <159387772+IND-SAB@users.noreply.github.com>
2024-02-08 06:03:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Cheaters","Listening to Admins","Playing with Tiers"];
 
 
 let currentIndex = 0;
